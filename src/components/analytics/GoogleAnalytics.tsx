63503b5bd32f695e19b439ca29dab0a26700e819
Iconic Usama <rao.usamaanwar@gmail.com>
2026-08-12 16:57:04 +0500

    Add Google Analytics 4 (G-XR0GD4TE31)

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,38 +1,26 @@
-// src/components/analytics/GoogleAnalytics.tsx
-'use client'
+"use client"
 
-import Script from 'next/script'
+import Script from "next/script"
 
-interface GoogleAnalyticsProps {
-  measurementId: string
-}
+const GA_MEASUREMENT_ID = "G-XR0GD4TE31"
 
-export default function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
-  if (!measurementId || process.env.NODE_ENV !== 'production') {
-    return null
-  }
-
+export default function GoogleAnalytics() {
   return (
     <>
       <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${measurementId}', {
-              page_path: window.location.pathname,
-              send_page_view: true,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   )
 }
